bff2a776e342c83eb3444b852892dcf202909175
Bombababa <136107476+Bombababa@users.noreply.github.com>
2024-08-16 19:04:46 +0300

    Update js.js

--- js.js
+++ js.js
@@ -10,6 +10,8 @@ function checkFields() {
     }
 }
 
+checkFields()
+
 function checkSum() {
     const num1 = parseFloat(document.getElementById('num1').value);
     const num2 = parseFloat(document.getElementById('num2').value);
